4f925259e587a33b9087ff0d733be56c69f76f34
BhaktiDesaiB <113396634+BhaktiDesaiB@users.noreply.github.com>
2023-10-25 20:57:49 -0400

    A6 (#10)
    * updating keypair
    
    * updated ami id
    
    * final

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const vpcCIDRBlock = new pulumi.Config("db_vpc").require("cidrBlock");
 const publicRouteTableCIDRBlock = new pulumi.Config("db_publicRouteTable").require("cidrBlock");
 const region = new pulumi.Config("aws").require("region");
 const db_keyName = new pulumi.Config("db_vpc").require("key");
+const dbName = new pulumi.Config("dbName").require("name");
+const dbPassword = new pulumi.Config("dbPassword").require("password");
+const dbUserName = new pulumi.Config("dbUserName").require("user");
 
 
 // Function for AWS availability zones
@@ -72,17 +75,19 @@ const createSubnets = async () => {
         const publicSubnetCIDRBlock = calculateSubnetCIDRBlock(vpcCIDRBlock, i + 10);
         const privateSubnetCIDRBlock = calculateSubnetCIDRBlock(vpcCIDRBlock, i + 15);
 
+
         // Create public subnet
         const publicSubnet = new aws.ec2.Subnet(`db_publicSubnet${i + 1}`, {
             vpcId: db_vpc.id,
             availabilityZone: availabilityZones[i],
             cidrBlock: publicSubnetCIDRBlock,
+            mapPublicIpOnLaunch: true,
             tags: {
                 Name: `db_publicSubnet${i + 1}`,
             },
         });
-
         db_publicSubnets.push(publicSubnet);
+
         // Create private subnet
         const privateSubnet = new aws.ec2.Subnet(`db_privateSubnet${i + 1}`, {
             vpcId: db_vpc.id,
@@ -117,7 +122,6 @@ const createSubnets = async () => {
                 protocol: "tcp",
                 cidrBlocks: ["0.0.0.0/0"], // Allow HTTPS from anywhere
             },
-            // Add ingress rule for your application port here
             {
                 fromPort: 3000,
                 toPort: 3000,
@@ -125,11 +129,19 @@ const createSubnets = async () => {
                 cidrBlocks: ["0.0.0.0/0"],
             }
         ],
+        egress: [
+            {
+                protocol: "-1", // -1 means all protocols
+                fromPort: 0,
+                toPort: 0, // Set both fromPort and toPort to 0 to allow all ports
+                cidrBlocks: ["0.0.0.0/0"],
+            },
+        ],
         tags: {
             Name: "appSecurityGroup",
         },
     });
-
+    
     for (let i = 0; i < db_publicSubnets.length; i++) {
         new aws.ec2.RouteTableAssociation(`db_publicRouteTableAssociation-${i}`, {
             subnetId: db_publicSubnets[i].id,
@@ -150,10 +162,120 @@ const createSubnets = async () => {
         });
     }
 
+    // ----------  RDS configuration starts here ...
+    // Create a security group for RDS instances
+
+    const databaseSecurityGroup = new aws.ec2.SecurityGroup("databaseSecurityGroup", {
+        vpcId: db_vpc.id,
+        ingress: [
+            // Add ingress rule for your application port
+            {
+                fromPort: 3306,
+                toPort: 3306,
+                protocol: "tcp",
+                securityGroups: [appSecurityGroup.id],
+                cidrBlocks: ["0.0.0.0/0"]
+            },
+        ],
+        egress: [
+             // Add egress rule for your application port
+             {
+                fromPort: 3306,
+                toPort: 3306,
+                protocol: "tcp",
+                securityGroups: [appSecurityGroup.id],
+                cidrBlocks: ["0.0.0.0/0"]
+            },
+        ]
+    });
+    await databaseSecurityGroup.id;
+    pulumi.log.info(
+        pulumi.interpolate`Database Security Group VPC ID: ${databaseSecurityGroup.id}`
+    );
+
+// Create an RDS parameter group
+    const rdsParameterGroup = new aws.rds.ParameterGroup("myRdsParameterGroup", {
+        vpcId: db_vpc.id,
+        family: "mariadb10.6", // Change this to match your database engine and version
+        name: "my-rds-parameter-group",
+        parameters: [
+            {
+                name: "character_set_server",
+                value: "utf8",
+            },
+            {
+                name: "collation_server",
+                value: "utf8_general_ci",
+            },
+        ],
+        tags: {
+            Name: "myRdsParameterGroup",
+        },
+    });
+
+    const rdsSubnetGroup = new aws.rds.SubnetGroup("rds_subnet_group", {
+                subnetIds: [db_privateSubnets[0].id, db_privateSubnets[1].id],
+                tags: {
+                    Name: "rdsSubnetGroup", // You can name it as desired
+                },
+            });
+
+ //RDS instance creation starts here...
+    const rdsInstance = new aws.rds.Instance("rds-instance", {
+        allocatedStorage: 20,
+        storageType: "gp2",
+        multiAz: false,
+        parameterGroupName: rdsParameterGroup.name,
+        identifier: "csye6225",
+        engine: "mariadb",
+        instanceClass: "db.t2.micro", // Choose the cheapest instance class
+        username: dbUserName,
+        password: dbPassword,
+        skipFinalSnapshot: true, // To avoid taking a final snapshot when deleting the RDS instance
+        publiclyAccessible: false, // Ensure it's not publicly accessible
+        dbSubnetGroupName: rdsSubnetGroup.name,
+        vpcSecurityGroupIds: [databaseSecurityGroup.id], //ec2Instance.id.vpcSecurityGroupIds --> this does not attach the databseSecurityGroup, 
+        // Attach the security group
+        // subnetIds: db_privateSubnets.map(subnet => subnet.id), // Use private subnets
+        dbName: dbName, // Database name
+        tags: {
+            Name: "rds-db-instance",
+        },
+    });
+    pulumi.log.info(
+        pulumi.interpolate`RDS instance id: ${rdsInstance.id}`
+    );
+
+    // user database configuration
+    const DB_HOST = pulumi.interpolate`${rdsInstance.address}`;
+    // User data script to configure the EC2 instance
+
+    const userData = pulumi.interpolate`#!/bin/bash
+ 
+    # Define the path to the .env file
+    envFile="/opt/csye6225/bhaktidesai_002701264_05/.env"
+     
+    # Check if the .env file exists
+    if [ -e "$envFile" ]; then
+      # If it exists, remove it
+      sudo rm "$envFile"
+    fi
+     
+    # Create the .env file
+    sudo touch "$envFile"
+    echo "DB_NAME='${rdsInstance.dbName}'" | sudo tee -a "$envFile"
+    echo "DB_HOST='${DB_HOST}'" | sudo tee -a "$envFile"
+    echo "DB_USERNAME='${rdsInstance.username}'" | sudo tee -a "$envFile"
+    echo "DB_PASSWORD='${rdsInstance.password}'" | sudo tee -a "$envFile"
+    echo "PORT='3306'" | sudo tee -a "$envFile"`;
+    pulumi.log.info(
+        pulumi.interpolate`DB data: DB_HOST, userDataScript - ${DB_HOST}, ${userData}`
+    );
+
     // EC2 Instance
     const ec2Instance = new aws.ec2.Instance("ec2Instance", {
         instanceType: "t2.micro", // Set the desired instance type
-        ami: "ami-09f39ff72f7f59a3a", // Replace with your custom AMI ID
+        ami: "ami-040c0c70318ff93ac", // Replace with your custom AMI ID
         vpcSecurityGroupIds: [appSecurityGroup.id],
         subnetId: db_publicSubnets[0].id, // Choose one of your public subnets
         vpcId: db_vpc.id,
@@ -162,9 +284,13 @@ const createSubnets = async () => {
             volumeSize: 25,
             volumeType: "gp2",
         },
+        protectFromTermination: false,
+        userData: userData, // Attach the user data script
         tags: {
             Name: "db_EC2Instance",
         },
     });
 };
-createSubnets(); //function to create subnets
+
+//function to create subnets
+createSubnets();
